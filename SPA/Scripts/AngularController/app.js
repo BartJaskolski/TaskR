898e1f22a082899fe425f5ff896e5505c376a5cc
BartJask <bart.jaskolski@wp.pl>
2018-05-27 00:39:27 +0200

    Add files via upload

---
+++ SPA/Scripts/AngularController/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("AngularForm", ["AngularForm.controllers", "ngRoute"]);
+
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+        when("/",
+        {
+            controller: "LoginController",
+            templateUrl: "/Partial/Login.html"
+        })
+    .otherwise({ redirectTo: "/" });
+}]);
---
+++ SPA/Scripts/AngularController/controller.js
@@ -0,0 +1,29 @@
+﻿angular.module("AngularForm.controllers", [])
+    .controller("LoginController", function ($scope, UserService) {
+        $scope.message = "Zaloguj się";
+
+        $scope.Login = function () {
+            UserService.loginMeth($scope.user);
+        }
+    })
+    .factory("UserService", ['$http', function ($http) {
+        var fac = {};
+        
+        fac.loginMeth = function(user)
+        {
+            $http.post("/api/User/UserLogin", user).then(function (response) {
+                alert(response.status);
+                console.log(response);
+                console.log(response.data.Data);
+                if(response.data.Data == null)
+                {
+                    alert("Błędne hasło lub login");
+                }
+                else
+                {
+                    alert("Zalogowano !" + response.data.Data.AccountType);
+                }
+            })
+        }
+        return fac;
+    }])
